2967089e7e57831015b903bcaecc529495b807ef
arpit yadav <arpityadav639338@gmai.com>
2026-08-12 12:35:52 +0530

    major-project

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,89 +1,128 @@
-const Listing=require("../models/listing");
+const Listing = require("../models/listing");
+const NodeGeocoder = require("node-geocoder");
 
-module.exports.index=async(req,res)=>{
-   const allListings= await Listing.find({});
-   res.render("listings/index.ejs",{allListings});
+const geocoder = NodeGeocoder({
+  provider: "openstreetmap",
+});
+
+// INDEX
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 };
 
-module.exports.renderNewForm=(req, res) => {
-    res.render("listings/new.ejs");
-}
+// RENDER NEW FORM
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new.ejs");
+};
 
-module.exports.showListing=async(req,res)=>{
-    let{id}=req.params;
-   const listing = await Listing.findById(id)
-   .populate({path:"reviews",
-      populate:{
-         path:"author",
+// SHOW LISTING
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
       },
-   })
-   .populate("owner");
-   if(!listing){
-    req.flash("error","Listing you requested for does not exist!");
-    res.redirect("/listings");
-   }
-   console.log(listing);
-   res.render("listings/show.ejs",{ listing} );
-}
+    })
+    .populate("owner");
 
-/*module.exports.createListing=async(req,res,next)=>{
-   let url=req.file.path;
-   let filename=req.file.filename;
-   const newListing = new Listing(req.body.listing);
-   newListing.owner = req.user._id;
-   newListing.image={url,filename};
-   await newListing.save();
-   req.flash("success","New Listing Created!");
-   res.redirect("/listings");
-   
-  
-}*/
-module.exports.createListing = async (req, res, next) => {
-   const newListing = new Listing(req.body.listing);
-   newListing.owner = req.user._id;
+  if (!listing) {
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
 
-   if (req.file) {
-      newListing.image = {
-         url: req.file.path,
-         filename: req.file.filename,
-      };
-   }
-
-   await newListing.save();
-   req.flash("success", "New Listing Created!");
-   res.redirect("/listings");
+  console.log(listing);
+  res.render("listings/show.ejs", { listing });
 };
 
-module.exports.renderEditForm=async(req,res)=>{
-    let{id}=req.params;
-   const listing = await Listing.findById(id);
-    if(!listing){
-    req.flash("error","Listing you requested for does not exist!");
+// CREATE LISTING
+module.exports.createListing = async (req, res, next) => {
+  try {
+    let data = await geocoder.geocode(
+      `${req.body.listing.location}, ${req.body.listing.country}`
+    );
+
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.geometry = {
+      type: "Point",
+      coordinates: [data[0].longitude, data[0].latitude],
+    };
+
+    if (req.file) {
+      newListing.image = {
+        url: req.file.path,
+        filename: req.file.filename,
+      };
+    }
+
+    await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
-   }
-   let originalImageUrl=listing.image.url;
-   originalImageUrl=originalImageUrl.replace("/upload","/upload/w_250");
+  } catch (err) {
+    console.error("Geocoding error:", err);
+    req.flash("error", "Could not find location. Please try again.");
+    res.redirect("/listings/new");
+  }
+};
 
-   res.render("listings/edit.ejs",{listing,originalImageUrl});
-}
+// RENDER EDIT FORM
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
 
-module.exports.updateListing=async(req,res)=>{
-    let{id}=req.params;
-   let listing=await Listing.findByIdAndUpdate(id,{...req.body.listing});
-   if(typeof req.file !=="undefined"){
-   let url =req.file.path;
-   let filename=req.file.filename;
-   listing.image={url,filename};
-   await listing.save();
-   }
-   req.flash("success","Listing Updated!");
-   res.redirect(`/listings/${id}`);
-}
+  if (!listing) {
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
 
-module.exports.destroyListing=async(req,res)=>{
-    let{id}=req.params;
-   let deletedListing=await Listing.findByIdAndDelete(id);
-   console.log(deletedListing);
-   req.flash("success","New Listing Deleted!");
-   res.redirect("/listings");
-}
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
+};
+
+// UPDATE LISTING
+module.exports.updateListing = async (req, res) => {
+  try {
+    let { id } = req.params;
+
+    let data = await geocoder.geocode(
+      `${req.body.listing.location}, ${req.body.listing.country}`
+    );
+
+    let listing = await Listing.findByIdAndUpdate(id, {
+      ...req.body.listing,
+    }, { new: true });
+
+    listing.geometry = {
+      type: "Point",
+      coordinates: [data[0].longitude, data[0].latitude],
+    };
+
+    if (typeof req.file !== "undefined") {
+      let url = req.file.path;
+      let filename = req.file.filename;
+      listing.image = { url, filename };
+    }
+
+    await listing.save();
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+  } catch (err) {
+    console.error("Geocoding error:", err);
+    req.flash("error", "Could not update location. Please try again.");
+    res.redirect("/listings");
+  }
+};
+
+// DELETE LISTING
+module.exports.destroyListing = async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
+};
